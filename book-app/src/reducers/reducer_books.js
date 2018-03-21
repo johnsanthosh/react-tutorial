30dc899724a17d79a65b7e8506f9a6b9df557a5d
John Santhosh <jsanthos@asu.edu>
2018-03-21 12:35:20 -0700

    Managing app state with redux. book-app.

---
+++ book-app/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+  return [
+    {title: "Javascript: The Good Parts", pages : 874},
+    {title: "Harry Potter", pages : 888},
+    {title: "The Lord Of The Rings", pages : 2342},
+    {title: "The Monk Who Sold His Ferrari", pages : 678}
+  ];
+}
